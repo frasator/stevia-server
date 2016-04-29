d779837302ef4aef3eb3ccc431bd2c4a9e0346d2
frasator <frasator@gmail.com>
2016-04-29 15:38:18 +0200

    Updated bin logs

--- bin/daemon-log.sh
+++ bin/daemon-log.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR="$( cd "$( dirname "$SCRIPT_DIR" )" && pwd )"
-DIRNAME = "$( dirname "$SCRIPT_DIR" )"
+DIRNAME = "$( dirname "${BASH_SOURCE[0]}" )"
 
 tail -f ~/.forever/"$DIRNAME-daemon.log" -n 200
--- bin/start-dev-daemon.sh
+++ bin/start-dev-daemon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR="$( cd "$( dirname "$SCRIPT_DIR" )" && pwd )"
-DIRNAME = "$( dirname "$SCRIPT_DIR" )"
+DIRNAME = "$( dirname "${BASH_SOURCE[0]}" )"
 
 forever -w --watchDirectory $DIR -a -l ~/.forever/"$DIRNAME-daemon.log" --workingDir $DIR start "$DIR/daemon.js"
